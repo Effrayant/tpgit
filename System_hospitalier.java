9aac1997f9e7384e9e20bd2c45e2cbe4911a5a8e
Effrayant <adressepournimp@hotmail.com>
2024-03-27 11:23:52 +0100

    modif systeme.java

--- System_hospitalier.java
+++ System_hospitalier.java
@@ -34,12 +34,12 @@ public class System_hospitalier {
     }
 
     private String analyserRequete(Requête requête) {
-        
+//Renvoie "requête analysée" après
         return "requête analysée";
     }
 
     private String comparerDonnes() {
-        
+//Renvoie "cluster trouvé" après avoir comparer les donnes
         return "Cluster trouvé";
     }
 
